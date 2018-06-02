d2c35a7345c6d0707ff27e0a6c8aa6482f926396
Shane Hobson <shanehobson1@gmail.com>
2018-06-01 23:28:44 -0400

    Wired up nodeJS app for sending email on form submit

--- gulpfile.js
+++ gulpfile.js
@@ -4,38 +4,38 @@ const sass = require('gulp-sass');
 
 //Compile SASS and Inject Into browser
 gulp.task('sass', function() {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'public/scss/*.scss'])
     .pipe(sass())
-    .pipe(gulp.dest("src/css"))
+    .pipe(gulp.dest("public/css"))
     .pipe(browserSync.stream());
 });
 
 //Move JS files to src/js folder
 gulp.task('js', function() {
     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
-    .pipe(gulp.dest('src/js'))
+    .pipe(gulp.dest('public/js'))
     .pipe(browserSync.stream());
 });
 
 //Watch SASS and Serve
 gulp.task('serve', ['sass'], function() {
     browserSync.init({
-        server: "./src"
+        server: "./public"
     });
 
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'public/scss/*.scss'], ['sass']);
+    gulp.watch("public/*.html").on('change', browserSync.reload);
 });
 
 //Move Font Awesome CSS file and Fonts folder to our src file
 gulp.task('fa', function() {
     return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
-    .pipe(gulp.dest("src/css"));
+    .pipe(gulp.dest("public/css"));
 });
 
 gulp.task('fonts', function() {
     return gulp.src('node_modules/font-awesome/fonts/*')
-    .pipe(gulp.dest("src/fonts"));
+    .pipe(gulp.dest("public/fonts"));
 });
 
 //Create default Gulp task
---
+++ server.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
+const config = require('./config');
+const password = config.password;
+
+const port = 3000;
+
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+//load homepage
+app.get('/', function (req, res) {
+    res.send('index.html');
+});
+
+//middleware for parsing request body
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+//handler to respond to post request from client & send email
+app.post('/', (req, res) => {
+    console.log('Request received.');
+    console.log(req.body);
+    
+    const name = req.body.name;
+    const phone = req.body.number;
+    const email = req.body.email;
+    const message = req.body.message;
+
+    //define send email function
+    function sendEmail(req, res) {
+        
+        const transporter = nodemailer.createTransport({
+            service: 'Gmail',
+            auth: {
+                user: 'hobsonelectricinc@gmail.com', // Your email id
+                pass: password // Your password
+            }
+        });
+
+        const html = `
+            <h3>You've Received an Inquiry From ${name}</h3>
+            <p>Name: ${name}</p>
+            <p>Phone Number: ${phone}</p>
+            <p>Email Address: ${email}</p>
+            <p>Message: ${message}</p>
+        `;
+
+        const mailOptions = {
+            from: 'hobsonelectricinc@gmail.com', // sender address
+            to: 'shanehobson1@gmail.com', // list of receivers
+            subject: `New Website Inquiry from ${name}`, // Subject line...New Website Inquiry from ${name}
+            // text: text //, // plaintext body
+            html: html
+        };
+
+        transporter.sendMail(mailOptions, function(error, info) {
+            if(error){
+                console.log(error);
+                res.json({yo: 'error'});
+            }else{
+                console.log('Message sent: ' + info.response);
+                res.json({yo: info.response});
+            };
+        });
+    }
+
+    //call send email function
+    sendEmail(req, res);
+
+    //reply to browser so no error message pops up
+    res.send();
+});
+
+app.listen(port, function () {
+    console.log(`Server is up on ${port}`);
+});
