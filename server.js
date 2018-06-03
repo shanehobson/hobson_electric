f7df1974871926bf7e86aba7eb5a76f9b7ce8620
Shane Hobson <shanehobson1@gmail.com>
2018-06-03 18:43:36 -0400

    Fixed bug in server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/dist'));
 
 //load homepage
 app.get('/', (req, res) => {
